6323fa511286f93d954cd095414ae8bb7fe8e3f5
patro01 <patroprateek11@gmail.com>
2025-07-16 12:40:58 +0530

    obs3

--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
 terraform {
  backend "azurerm" {
    storage_account_name = "tfremotestoragepatro"
-   container_name = "tfremotestatecontain"
+   container_name = "tflockstoragehimcon1"
    resource_group_name = "tf-backend-lock"
    key = "githubactions/patro-terraform.tfstate"   
  }
